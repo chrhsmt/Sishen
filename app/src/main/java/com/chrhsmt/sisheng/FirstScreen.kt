76ee968c0c33f4f0d712640a40fb55ee3b8b651d
tera <terry7188@gmail.com>
2017-09-27 08:08:56 +0900

    add third screen and refactor activity name.

--- app/src/main/java/com/chrhsmt/sisheng/FirstScreen.kt
+++ app/src/main/java/com/chrhsmt/sisheng/FirstScreen.kt
@@ -3,9 +3,6 @@ package com.chrhsmt.sisheng
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.view.View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-import android.view.View.SYSTEM_UI_FLAG_FULLSCREEN
-import android.view.View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
 import android.content.Intent
 // 画面用にこれをimport しておく
 import kotlinx.android.synthetic.main.activity_first_screen.*
@@ -21,11 +18,19 @@ class FirstScreen : AppCompatActivity() {
 
         // ランダム例文に移動
         btnRandom.setOnClickListener(View.OnClickListener {
+            //todo: ランダムに例文を渡す処理
+
             val intent = Intent(this@FirstScreen,
-                    RandomReibunActivity::class.java)
+                    ReibunActivity::class.java)
             startActivity(intent)
         })
 
+        // 任意例文に移動
+        btnNini.setOnClickListener(View.OnClickListener {
+            val intent = Intent(this@FirstScreen,
+                    NiniReibunActivity::class.java)
+            startActivity(intent)
+        })
 
     }
 
